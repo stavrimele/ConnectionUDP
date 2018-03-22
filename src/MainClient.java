c381038b27ae0a170bf087a2d449fb64f0d4a94d
stavrimele <stavrimele.sm@gmail.com>
2018-03-22 10:39:28 +0100

    Richiesta data UDP

---
+++ src/MainClient.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author christian
+ */
+public class MainClient {
+    public static void main(String[] args) 
+    {
+        Client c=new Client();
+        c.connessione();
+    }
+}
---
+++ src/MainServer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author christian
+ */
+public class MainServer {
+    public static void main(String[] args) 
+    {
+        Server s=new Server();
+        s.connessione();
+    }
+}
